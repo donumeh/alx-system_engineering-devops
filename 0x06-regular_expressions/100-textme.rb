f6b23357b88040c58c1c22bc9f85e926cb89136e
Donald <user@example.com>
2024-02-27 16:21:00 +0100

    read log file and output sender receiver and flag info

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby
 
-ARGF.each_line do |line|
-  
-  sender = line.match(/\[from:(?<sender>.*?)\]/)[:sender]
-  receiver = line.match(/\[to:(?<receiver>.*?)\]/)[:receiver]
-  flags = line.match(/\[flags:(?<flags>.*?)\]/)[:flags]
-
-  
-  puts "#{sender},#{receiver},#{flags}"
+# Extract sender, receiver, and flags using regular expressions
+def extract_info(log_entry)
+  sender = log_entry.match(/\[from:(?<sender>.*?)\]/)[:sender]
+  receiver = log_entry.match(/\[to:(?<receiver>.*?)\]/)[:receiver]
+  flags = log_entry.match(/\[flags:(?<flags>.*?)\]/)[:flags]
+  "#{sender},#{receiver},#{flags}"
 end
+
+# Accept log entry string from command line argument
+log_entry = ARGV[0]
+
+# Output extracted information
+puts extract_info(log_entry)
 
