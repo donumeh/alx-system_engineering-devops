847adb3f0f9e1b398e9ee4409c1badbce82ca4ef
Donald <donumeh54@gmail.com>
2024-07-07 16:25:43 +0100

    setup ssh config using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,42 +1,41 @@
 # Using a puppet to make changes to the config file
 
-file {'/home/ubuntu/.ssh':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0700',
-}
-
-file {'/home/ubuntu/.ssh/config':
-  ensure  => present,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  content => '',
+file {'/etc/ssh/ssh_config':
+  ensure => present,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
 }
 
 file_line { 'Add Host block':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'Host myserver',
   match => '^Host myserver',
 }
 
-file line {'Add HostName':
-  path  => '/home/ubuntu/.ssh/config',
+file_line {'Add HostName':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    HostName 98.98.98.98',
+  match => '^\s*HostName\s+',
+  after => '^Host myserver',
+}
+
+file_line {'Add User':
+  path  => '/etc/ssh/ssh_config',
   line  => '    User ubuntu',
   match => '^\s*User\s+',
   after => '^Host myserver',
 }
 
 first_line {'Add IdentityFile':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => '    IdentityFile /home/ubuntu/.ssh/school',
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
   match => '^\s*IdentityFile\s+',
   after => '^Host myserver',
 }
 
 file_line {'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => '    PasswordAuthentication no',
   match => '^\s*PasswordAuthentication\s+',
   after => '^Host myserver',
