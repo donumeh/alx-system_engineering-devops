3132377a4d33070eacd71bd6d2292802cd243072
Donald <donumeh54@gmail.com>
2024-08-13 12:37:55 +0100

    readme and number of subscribers function

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Request for an info from Reddit api """
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Function that request for number of subscribers in a reddit channel
+    args:
+        subreddit: string
+
+    return: 0 if not a valud subreddit, else number of follower
+    """
+    # Define the base URL
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # Set custom User-Agent to avoid Too Many Requests error
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    try:
+        # Make a GET request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            # Return the number of subscribers
+            return data.get("data", {}).get("subscribers", 0)
+        else:
+            # Return 0 if the subreddit is invalid or any other issue occurs
+            return 0
+    except requests.RequestException:
+        # Handle any request exceptions
+        return 0
