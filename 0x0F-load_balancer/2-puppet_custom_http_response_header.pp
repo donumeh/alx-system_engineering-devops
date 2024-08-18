ad8af1e39084f401f1bed48282ec1c8feba8b66e
Donald <donumeh54@gmail.com>
2024-08-18 12:10:42 +0100

    use puppet to add header in web server

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+package {'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+
+	server_name _;
+
+	location / {
+		add_header X-Served-By $hostname;
+		try_files $uri $uri/ =404;
+	}
+}
+',
+  notify  => Service['nginx'],
+}
+
+service {'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
